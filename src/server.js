d0c348637d564d144421a573f8b72a22774917cb
Noushath Moidu <nmoidu@deloitte.com>
2021-01-06 12:26:24 +0530

    fixing missed variable

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const {connectDB} = require("./db/setUpDb")
+const {connectDB} = require("./db/setUpDb");
 const express = require("express");
 const employeeRouter = require("./router/employeeRouter");
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use("/employee",employeeRouter);
 app.set("port", process.env.PORT || 3000);
-app.listen(app.get("port"), async function () {
+const server = app.listen(app.get("port"), async function () {
     await connectDB();
     console.log("Express server listening on port " + server.address().port)
 });
